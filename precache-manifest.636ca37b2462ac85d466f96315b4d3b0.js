76772ab895f3d8ceb667348da53cc20d6e80cdb2
WenKang <wenkang11861@gmail.com>
2020-11-10 21:33:03 +0800

    Updates

---
+++ precache-manifest.636ca37b2462ac85d466f96315b4d3b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887cf50cb5aeb396d4a23a5ed48f448d",
+    "url": "/lim/index.html"
+  },
+  {
+    "revision": "8d2abada0138fafafae3",
+    "url": "/lim/static/css/main.b537f780.chunk.css"
+  },
+  {
+    "revision": "6efd157b12ff9405e56b",
+    "url": "/lim/static/js/2.c23c0f58.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/lim/static/js/2.c23c0f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d2abada0138fafafae3",
+    "url": "/lim/static/js/main.ec2c04a0.chunk.js"
+  },
+  {
+    "revision": "6988ae675598415fe04b",
+    "url": "/lim/static/js/runtime-main.444568f7.js"
+  }
+]);
